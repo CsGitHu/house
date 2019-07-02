a6d69055aae9d0da2bbe35f51568123758979ce0
CsGitHu <1152377518@qq.com>
2019-07-02 14:33:58 +0800

    小陈的项目

--- src/main/java/com/whhp/controller/a.java
+++ src/main/java/com/whhp/controller/a.java
@@ -4,9 +4,9 @@ package com.whhp.controller;
  * @author Administrator
  * @Date: 2019/7/2
  * @Created with IntelliJ IDEA.
- * @Time: 14:22
+ * @Time: 14:32
  * @To change this template use File | Settings | File Templates.
  * @Description:
  */
-public class aaaa {
+public class a {
 }
---
+++ src/main/java/com/whhp/controller/b.java
@@ -0,0 +1,12 @@
+package com.whhp.controller;
+
+/**
+ * @author Administrator
+ * @Date: 2019/7/2
+ * @Created with IntelliJ IDEA.
+ * @Time: 14:32
+ * @To change this template use File | Settings | File Templates.
+ * @Description:
+ */
+public class b {
+}
