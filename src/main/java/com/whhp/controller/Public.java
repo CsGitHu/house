997dd2f9ac87cd398211ef07707ad61afe5e5847
CsGitHu <1152377518@qq.com>
2019-07-02 14:59:05 +0800

    Merge remote-tracking branch 'test/master'
    # Conflicts:
    #	src/main/java/com/whhp/controller/a.java

---
+++ src/main/java/com/whhp/controller/Public.java
@@ -0,0 +1,15 @@
+package com.whhp.controller;
+
+/**
+ * @author Administrator
+ * @Date: 2019/7/2
+ * @Created with IntelliJ IDEA.
+ * @Time: 14:36
+ * @To change this template use File | Settings | File Templates.
+ * @Description:
+ */
+public class Public {
+	public static void main(String[] args) {
+		System.out.println("args = " + args);
+	}
+}
